93681a0ccfcdb76c2c92b3b14993f27ab38b6b8f
reemrev21 <reemrev21@gmail.com>
2022-05-12 23:56:09 +0900

    [add] sell setting

---
+++ src/pages/Sell.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Wrap = styled.div`
+  width: 100%;
+`
+const Sell = () => {
+  return (
+    <Wrap>
+      <div>Sell</div>
+      <div>판매하기</div>
+      {/* <button></button> */}
+    </Wrap>
+  )
+}
+
+export default Sell
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/Main'
+import SellPage from './pages/Sell'
 
 const RouteList = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/sell" element={<SellPage />} />
     </Routes>
   )
 }
