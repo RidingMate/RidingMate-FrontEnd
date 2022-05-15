4bb0ec794552ebb33a9a45953d1a1aa4b506552d
reemrev21 <reemrev21@gmail.com>
2022-05-15 12:22:38 +0900

    [add] market card view

--- src/pages/market/Market.js
+++ src/pages/market/Market.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import styled from 'styled-components'
 import Layout from '../../components/market/Layout'
 import Card from '../../components/market/Card'
@@ -8,7 +8,7 @@ import RightArrow from './rightArrowIcon.svg'
 // import { useNavigate } from 'react-router-dom'
 
 const Wrap = styled.div`
-  width: 60%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -17,7 +17,7 @@ const Wrap = styled.div`
 `
 
 const NoticeContainer = styled.div`
-  width: 100%;
+  width: 40%;
 
   display: flex;
   justify-content: space-between;
@@ -42,16 +42,32 @@ const NoticeContainer = styled.div`
     cursor: pointer;
   }
 `
+const SearchContainer = styled.div`
+  width: 40%;
+  margin-bottom: 105px;
+`
+
+const ContentContainer = styled.div`
+  width: 70%;
+  display: flex;
+`
+
+const FilterContainer = styled.div`
+  width: 315px;
+  height: 100vh;
+  border: 1px solid black;
+  margin-right: 45px;
+`
 
 const MyBikeArea = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  /* align-items: center; */
 `
 
 const MyBikeContainer = styled.div`
   :not(:last-of-type) {
-    margin-right: 80px;
+    margin-right: 40px;
   }
 `
 
@@ -82,23 +98,52 @@ const myBike = [
   {
     key: 'sss1',
     title: '판매글 제목입니다. 1',
-    cc: '000cc',
-    km: '00,000km',
+    cc: '000',
+    km: '00000',
     subTitle: '혼다 Super Cub',
     img: 'https://velog.velcdn.com/images%2Freemrev21%2Fpost%2F3a760ad5-c0e4-457e-8f15-5c7d9f986afc%2F72df9a1e-1b6c-4fff-aed5-0b21a9b685f0.jpg',
+    price: 3150000,
   },
   {
     key: 'sss2',
     title: '판매글 제목입니다. 2',
-    cc: '000cc',
-    km: '00,000km',
+    cc: '000',
+    km: '00000',
     subTitle: '혼다 Super Cub',
     img: 'https://velog.velcdn.com/images%2Freemrev21%2Fpost%2F3a760ad5-c0e4-457e-8f15-5c7d9f986afc%2F72df9a1e-1b6c-4fff-aed5-0b21a9b685f0.jpg',
+    price: 2520000,
+  },
+  {
+    key: 'sss3',
+    title: '판매글 제목입니다. 3',
+    cc: '000',
+    km: '00000',
+    subTitle: '혼다 Super Cub',
+    img: 'https://velog.velcdn.com/images%2Freemrev21%2Fpost%2F3a760ad5-c0e4-457e-8f15-5c7d9f986afc%2F72df9a1e-1b6c-4fff-aed5-0b21a9b685f0.jpg',
+    price: 20600000,
+  },
+  {
+    key: 'sss4',
+    title: '판매글 제목입니다. 4',
+    cc: '000',
+    km: '00000',
+    subTitle: '혼다 Super Cub',
+    img: 'https://velog.velcdn.com/images%2Freemrev21%2Fpost%2F3a760ad5-c0e4-457e-8f15-5c7d9f986afc%2F72df9a1e-1b6c-4fff-aed5-0b21a9b685f0.jpg',
+    price: 315000000,
+  },
+  {
+    key: 'sss5',
+    title: '판매글 제목입니다. 5',
+    cc: '000',
+    km: '000000',
+    subTitle: '혼다 Super Cub',
+    img: 'https://velog.velcdn.com/images%2Freemrev21%2Fpost%2F3a760ad5-c0e4-457e-8f15-5c7d9f986afc%2F72df9a1e-1b6c-4fff-aed5-0b21a9b685f0.jpg',
+    price: 315000000,
   },
 ]
 
 const Market = () => {
-  const [selected, setSelected] = useState([])
+  // const [selected, setSelected] = useState([])
   // const navigate = useNavigate()
 
   const selectMarketBike = (title) => {
@@ -115,22 +160,29 @@ const Market = () => {
           </div>
           <img src={RightArrow} alt="오른쪽 버튼" />
         </NoticeContainer>
-        <SearchForm />
-        <MyBikeArea>
-          {myBike.map((item) => (
-            <MyBikeContainer
-              key={item.key}
-              onClick={() => selectMarketBike(item.title)}
-            >
-              <Card
-                title={item.title}
-                subTitle={item.subTitle}
-                img={item.img}
-                selected={selected}
-              />
-            </MyBikeContainer>
-          ))}
-        </MyBikeArea>
+        <SearchContainer>
+          <SearchForm />
+        </SearchContainer>
+        <ContentContainer>
+          <FilterContainer></FilterContainer>
+          <MyBikeArea>
+            {myBike.map((item) => (
+              <MyBikeContainer
+                key={item.key}
+                onClick={() => selectMarketBike(item.title)}
+              >
+                <Card
+                  title={item.title}
+                  subTitle={item.subTitle}
+                  cc={item.cc}
+                  km={item.km}
+                  img={item.img}
+                  price={item.price}
+                />
+              </MyBikeContainer>
+            ))}
+          </MyBikeArea>
+        </ContentContainer>
       </Wrap>
     </Layout>
   )
